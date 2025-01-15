083ce9ebafd85e4e475b9b4a19bc7a0079708c7d
jtqx <147728729+jtqx@users.noreply.github.com>
2025-01-15 23:44:04 +0800

    Update UserProfile.java

--- src/Entities/UserProfile.java
+++ src/Entities/UserProfile.java
@@ -1,132 +1,113 @@
 package Entities;
 
-// Java imports
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Vector;
-
-// Class imports
-import util.MySQLAccess;
 
 public class UserProfile {
 
     public Map<String, String> getUserProfile(String role) {
         Map<String, String> infoMap = null;
-        try {
-            MySQLAccess sqlAccess = new MySQLAccess();
-            String query = "SELECT * FROM userprofile WHERE role = ?";
-            PreparedStatement statement = sqlAccess.prepareStatement(query);
-            statement.setString(1, role);
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            String hql = "FROM UserProfileEntity WHERE role = :role";
+            Query<UserProfileEntity> query = session.createQuery(hql, UserProfileEntity.class);
+            query.setParameter("role", role);
+            UserProfileEntity userProfile = query.uniqueResult();
+
+            if (userProfile != null) {
                 infoMap = new HashMap<>();
-                infoMap.put("role", resultSet.getString("role"));
-                infoMap.put("description", resultSet.getString("description"));
-                infoMap.put("isSuspended", resultSet.getString("isSuspended"));
+                infoMap.put("role", userProfile.getRole());
+                infoMap.put("description", userProfile.getDescription());
+                infoMap.put("isSuspended", String.valueOf(userProfile.isSuspended()));
             }
-            sqlAccess.close();
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
-        catch (ClassNotFoundException e) {
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
             e.printStackTrace();
         }
         return infoMap;
     }
 
-    public Vector<String> getRoles() throws ClassNotFoundException, SQLException {
-        Vector<String> roles = new Vector<String>();
-        // connect to the database and query for data
-        try {
-            MySQLAccess sqlAccess = new MySQLAccess();
-            String query = "SELECT role FROM userprofile";
-            PreparedStatement statement = sqlAccess.prepareStatement(query);
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                String role = resultSet.getString("role");
-                roles.add(role);
-            }
-            sqlAccess.close();
-        }
-        catch (SQLException e) {
+    public Vector<String> getRoles() {
+        Vector<String> roles = new Vector<>();
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            String hql = "SELECT role FROM UserProfileEntity";
+            Query<String> query = session.createQuery(hql, String.class);
+            List<String> resultList = query.list();
+
+            roles.addAll(resultList);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
             e.printStackTrace();
         }
-
         return roles;
     }
 
-    public boolean insertToDB(String role, String description, String isSuspended) throws ClassNotFoundException {
-        // connect to database and query an insert statement
-        try {
-            MySQLAccess sqlAccess = new MySQLAccess();
-            String query = "INSERT INTO userprofile VALUES(?, ?, ?)";
-            PreparedStatement statement = sqlAccess.prepareStatement(query);
-            statement.setString(1, role);
-            statement.setString(2, description);
-            statement.setString(3, isSuspended);
-            boolean status = sqlAccess.executePreparedStatement(statement);     // get the status of the query
-            sqlAccess.close();          // close the connection
-            return status;
-        }
-        catch (SQLException e) {
+    public boolean insertToDB(String role, String description, boolean isSuspended) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            UserProfileEntity userProfile = new UserProfileEntity(role, description, isSuspended);
+            session.save(userProfile);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
             e.printStackTrace();
             return false;
         }
     }
 
-    public boolean updateDB(String role, String updatedDesc) throws ClassNotFoundException {
-        // connect to database and query an update statement
-        try {
-            MySQLAccess sqlAccess = new MySQLAccess();
-            String query = "UPDATE userprofile SET description = ? WHERE role = ?";
-            PreparedStatement statement = sqlAccess.prepareStatement(query);
-            statement.setString(1, updatedDesc);
-            statement.setString(2, role);
-            boolean status = sqlAccess.executePreparedStatement(statement);     // get the status of the query
-            sqlAccess.close();          // close the connection
-            return status;
-        }
-        catch (SQLException e) {
+    public boolean updateDB(String role, String updatedDesc) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            String hql = "UPDATE UserProfileEntity SET description = :description WHERE role = :role";
+            Query<?> query = session.createQuery(hql);
+            query.setParameter("description", updatedDesc);
+            query.setParameter("role", role);
+            int updatedRows = query.executeUpdate();
+            transaction.commit();
+            return updatedRows > 0;
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
             e.printStackTrace();
             return false;
         }
     }
 
-    public boolean suspendUserProfile(String role) throws ClassNotFoundException {
-        // connect to database and query an update statement
-        try {
-            MySQLAccess sqlAccess = new MySQLAccess();
-            String query = "UPDATE userprofile SET isSuspended = ? WHERE role = ?";
-            PreparedStatement statement = sqlAccess.prepareStatement(query);
-            statement.setString(1, "1");
-            statement.setString(2, role);
-            boolean status = sqlAccess.executePreparedStatement(statement);     // get the status of the query
-            sqlAccess.close();          // close the connection
-            return status;
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
+    public boolean suspendUserProfile(String role) {
+        return updateSuspensionStatus(role, true);
     }
 
-    public boolean reinstateUserProfile(String role) throws ClassNotFoundException {
-        // connect to database and query an update statement
-        try {
-            MySQLAccess sqlAccess = new MySQLAccess();
-            String query = "UPDATE userprofile SET isSuspended = ? WHERE role = ?";
-            PreparedStatement statement = sqlAccess.prepareStatement(query);
-            statement.setString(1, "0");
-            statement.setString(2, role);
-            boolean status = sqlAccess.executePreparedStatement(statement);     // get the status of the query
-            sqlAccess.close();          // close the connection
-            return status;
-        }
-        catch (SQLException e) {
+    public boolean reinstateUserProfile(String role) {
+        return updateSuspensionStatus(role, false);
+    }
+
+    private boolean updateSuspensionStatus(String role, boolean isSuspended) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            String hql = "UPDATE UserProfileEntity SET isSuspended = :isSuspended WHERE role = :role";
+            Query<?> query = session.createQuery(hql);
+            query.setParameter("isSuspended", isSuspended);
+            query.setParameter("role", role);
+            int updatedRows = query.executeUpdate();
+            transaction.commit();
+            return updatedRows > 0;
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
             e.printStackTrace();
             return false;
         }
